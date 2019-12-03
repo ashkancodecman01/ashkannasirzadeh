b8d77eb3ad2c50bd8f3fb6f2b570500498acd701
ashkan-nasirzadeh <ashkan.codecman.01@gmail.com>
2019-12-03 06:35:46 +0330

    sitemap added

---
+++ sitemap/index.js
@@ -0,0 +1,45 @@
+jQuery(document).ready(function($) {
+    var array = [
+        ['1', 'MyThinks', ''],
+        ['2', 'ashkan-nasirzadeh.ir', 'http://www.ashkan-nasirzadeh.ir/'],
+        ['3', 'sitemap', '']
+    ];
+
+
+    function textInner(array = []) {
+        array.forEach(function(v, i) {
+            if (v[1] != '' && v[2] != '') {
+                $('*[data-txt="txt' + v[0] + '"]').prepend(v[1] + `<a href='${v[2]}' target='_blank'> &#128279;</a>`);
+            } else {
+                $('*[data-txt="txt' + v[0] + '"]').prepend(v[1]);
+            }
+        });
+    }
+
+
+    $('#nav > div').click(function() {
+        let dataListChilds = $(this).attr('data-list-childs');
+        let motherMarginLeft = parseInt($(this).css('margin-left'));
+
+        if (dataListChilds !== undefined) {
+            $('*[data-list-parent]').each(function(i, v) {
+                let dataListParent = $(this).attr('data-list-parent');
+                if (dataListParent == dataListChilds) {
+                    $(this).css('display', $(this).css('display') == 'none' ? 'block' : 'none');
+                    let marginLeft = $(this).css('display') == 'none' ? 0 : motherMarginLeft + 30;
+                    $(this).css('margin-left', marginLeft);
+                }
+            });
+        }
+    });
+    textInner(array);
+
+});
+
+function open_closeAll() {
+    let temp = document.querySelectorAll('[data-txt]');
+    temp.forEach(function(v, i) {
+        console.log(v);
+        v.click();
+    });
+}
