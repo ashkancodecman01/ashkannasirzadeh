c5fff0a661d1635b2bd6e119b74d381a59a7824a
ashkan-nasirzadeh <ashkan.codecman.01@gmail.com>
2020-03-04 01:05:18 +0330

    vmware added

---
+++ assets/divar/divar.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function ($) {
+    $('#passBox').focus();
+    $('#passBox').on('keydown', function (e) {
+        if (e.keyCode == 13) {
+            $('#sendBtn').click();
+        }
+    });
+    $('#sendBtn').click(function () {
+        let pass = $('#passBox').val();
+        if (pass == 'ashi') {
+            $('.hide-c').css('display', 'none');
+        } else {
+            $('#passBox').val('');
+        }
+    });
+});
