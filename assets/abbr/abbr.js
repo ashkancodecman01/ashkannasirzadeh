9a59c6975a2f2184fae825a5a0a866c3326a8ff2
ashkan-nasirzadeh <ashkan.codecman.01@gmail.com>
2020-04-01 14:34:15 +0430

    -

--- assets/abbr/abbr.js
+++ assets/abbr/abbr.js
@@ -1,10 +1,11 @@
 $basicArray = [
     {"phrase": "powershell ISE", "abbr": "ISE", "standFor": "Integrated Scripting Environment"},
+    {"phrase": "HTTP messages are passed in a format similar to that used by the Internet mail [RFC5322] and the MIME [RFC2045]", "abbr": "MIME", "standFor": "Multipurpose Internet Mail Extensions"},
     {"phrase": "http", "abbr": "http", "standFor": "Hypertext Transfer Protocol"},
     {"phrase": "URI", "abbr": "URI", "standFor": "Uniform Resource Identifire"},
-    {"phrase": "java SE", "abbr": "SE", "standFor": "standard edition"},
+    {"phrase": "URL", "abbr": "URL", "standFor": "Uniform Resource Locator"},
+    {"phrase": "URN", "abbr": "URN", "standFor": "Uniform Resource Name"},
     {"phrase": "http server:generally a web server like Apache Web Server or IIS", "abbr": "IIS", "standFor": "Internet Information Services"},
-    {"phrase": "HTTP messages are passed in a format similar to that used by the Internet mail [RFC5322] and the MIME [RFC2045]", "abbr": "MIME", "standFor": "Multipurpose Internet Mail Extensions"},
 ]
 jQuery(document).ready(function ($) {
     $basicArray.forEach(function (v, i) {
